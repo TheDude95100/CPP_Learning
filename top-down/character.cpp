65728d45077b04b8f880487c4bfe3bad71caa6c6
TheDude95100 <hcalvet70@gmail.com>
2025-05-01 22:00:45 -0400

    Top down

---
+++ top-down/character.cpp
@@ -0,0 +1,62 @@
+#include "character.h"
+#include "raymath.h"
+Character::Character(int winWidth, int winHeight)
+{
+    width = texture.width / maxFrames;
+    height = texture.height;
+
+    screenPos = {static_cast<float>(winWidth) / 2.0f - scale * (0.5f * width),
+                 static_cast<float>(winHeight) / 2.0f - scale * (0.5f * height)};
+}
+void Character::unloadTexture()
+{
+    UnloadTexture(texture);
+    UnloadTexture(idle);
+    UnloadTexture(run);
+}
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+    screenPos = {(float)winWidth / 2.0f - scale * (0.5f * width / 6.0f),
+                 (float)winHeight / 2.0f - scale * (0.5f * height)};
+}
+void Character::tick(float deltaTime)
+{
+    worldPosLastFrame = worldPos;
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1;
+    if (Vector2Length(direction) != 0.0)
+    {
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+    Rectangle source{frame * (float)texture.width / 6.f, 0.f, rightLeft * width, height};
+    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+}
+
+void Character::undoMovement()
+{
+    worldPos = worldPosLastFrame;
+}
---
+++ top-down/top_down.cpp
@@ -0,0 +1,42 @@
+#include "raylib.h"
+#include "raymath.h"
+#include "character.h"
+
+int main()
+{
+    const int windowHeight{384};
+    const int windowWidth{384};
+
+    const float mapScale{4.f};
+
+    InitWindow(windowWidth, windowHeight, "top down");
+
+    Texture2D background = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+
+    Vector2 mapPos{0.0, 0.0};
+
+    Character knight(windowWidth,windowHeight);
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+        mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
+        DrawTextureEx(background, mapPos, 0.0, mapScale, WHITE);
+        knight.tick(GetFrameTime());
+        if (knight.getWorldPos().x < 0.f ||
+            knight.getWorldPos().y < 0.f ||
+            knight.getWorldPos().x + windowWidth > background.width * mapScale ||
+            knight.getWorldPos().y + windowHeight > background.height * mapScale)
+            {
+                knight.undoMovement();
+            }
+
+        EndDrawing();
+    }
+
+    UnloadTexture(background);
+    knight.unloadTexture();
+}
